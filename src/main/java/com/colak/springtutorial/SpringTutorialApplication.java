6a317ced6af6f9632287b650b694a2a929568d60
Orçun Çolak <orcun.colak@hazelcast.com>
2024-11-11 09:47:13 +0300

    upgrade spring

--- src/main/java/com/colak/springtutorial/SpringTutorialApplication.java
+++ src/main/java/com/colak/springtutorial/SpringTutorialApplication.java
@@ -1,13 +1,13 @@
-package com.colak.springflywaytutorial;
+package com.colak.springtutorial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringFlywayTutorialApplication {
+public class SpringTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringFlywayTutorialApplication.class, args);
+		SpringApplication.run(SpringTutorialApplication.class, args);
 	}
 
 }
